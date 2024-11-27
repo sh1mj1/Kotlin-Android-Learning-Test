2b742fd89aa539f1dde458c8b688c4d3c9863633
sh1mj1 <7wlgns@gmail.com>
2024-11-28 08:48:43 +0900

    build: ktlint dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    dependencies {
+        classpath(libs.ktlint)
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    tasks {
+        withType<Test> {
+            useJUnitPlatform()
+        }
+
+        withType<KotlinCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_17.toString()
+            }
+        }
+    }
 }
