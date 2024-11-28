85563a3b65366ce3241f6e7729b5cfcc3400af4c
sh1mj1 <7wlgns@gmail.com>
2024-11-28 16:13:28 +0900

    build: organize dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -26,13 +25,16 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
+
     packaging {
         resources {
             excludes += "META-INF/**"
@@ -42,32 +44,9 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation(libs.bundles.androidx)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 
-    testImplementation(libs.junit.jupiter)
-    testRuntimeOnly(libs.junit.vintage.engine)
-    testImplementation(libs.kotest.runner.junit5)
-    testImplementation(libs.android.test.runner)
-
-    // Kotest for Android Instrumentation Tests
-    androidTestImplementation(libs.kotest.runner.junit5)
-    androidTestImplementation(libs.android.test.runner)
-    androidTestImplementation(libs.junit.jupiter)
-
-    // Required for Kotest & JUnit 5 integration
-    androidTestRuntimeOnly(libs.junit.vintage.engine)
-    testImplementation(libs.robolectric)
-
-    testImplementation(libs.mockk)
-    testImplementation(libs.mockk.agent)
-    androidTestImplementation(libs.mockk.android)
-    androidTestImplementation(libs.mockk.agent)
+    testImplementation(libs.bundles.test)
+    androidTestImplementation(libs.bundles.androidTest)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     dependencies {
         classpath(libs.ktlint)
     }
 }
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
