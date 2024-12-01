b65ddd7ee450ca4eb8d1036ec2d4fff458cec4b4
sh1mj1 <7wlgns@gmail.com>
2024-12-01 15:19:13 +0900

    test(KotestFunSpec): basic FunSpec

---
+++ app/src/test/java/com/example/learningtest/kotest/KotestFunSpec.kt
@@ -0,0 +1,18 @@
+package com.example.learningtest.kotest
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+/**
+ * FunSpec allows you to create tests by invoking a function called test with a string argument to describe the test, and then the test itself as a lambda.
+ */
+class KotestFunSpec : FunSpec({
+    test("the sum of 2 and 3 is 5") {
+        2 + 3 shouldBe 5
+    }
+
+    test("String length should return the length of the string") {
+        "sammy".length shouldBe 5
+        "".length shouldBe 0
+    }
+})
