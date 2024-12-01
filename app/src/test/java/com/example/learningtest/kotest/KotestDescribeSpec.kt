dc547ab05a1204e24597f91edcf16adbaf7c8e19
sh1mj1 <7wlgns@gmail.com>
2024-12-01 15:29:58 +0900

    test(KotestDescribeSpec): basic DescribeSpec

---
+++ app/src/test/java/com/example/learningtest/kotest/KotestDescribeSpec.kt
@@ -0,0 +1,28 @@
+package com.example.learningtest.kotest
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+/**
+ * DescribeSpec offers a style familiar to those from a Ruby or Javascript background, as this testing style uses `describe` / `it` keywords.
+ *
+ * Tests must be nested in one or more describe blocks.
+ */
+class KotestDescribeSpec : DescribeSpec({
+    describe("the sum of 2 and 3") {
+        it("should be 5") {
+            2 + 3 shouldBe 5
+        }
+        it("should not be 6") {
+            2 + 3 shouldNotBe 6
+        }
+    }
+
+    describe("strings.length") {
+        it("should return the size of the string") {
+            "sammy".length shouldBe 5
+            "".length shouldBe 0
+        }
+    }
+})
