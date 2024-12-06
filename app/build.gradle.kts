10154da7db8010b69506a2beeb1b7eab20259222
sh1mj1 <7wlgns@gmail.com>
2024-12-06 15:16:21 +0900

    build: compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -41,6 +42,10 @@ android {
             excludes += "win32-x86*/**"
         }
     }
+}
+
+composeCompiler {
+    enableStrongSkippingMode = true
 }
 
 dependencies {
