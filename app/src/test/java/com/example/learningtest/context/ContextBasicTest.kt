b0d3dfaeba01f8b0d92fb91bfd106045b491f5b2
sh1mj1 <7wlgns@gmail.com>
2024-12-30 01:04:08 +0900

    test: can get android resources such as string, drawable through context

--- app/src/test/java/com/example/learningtest/context/ContextBasicTest.kt
+++ app/src/test/java/com/example/learningtest/context/ContextBasicTest.kt
@@ -8,6 +8,9 @@ import android.content.Intent
 import android.os.IBinder
 import androidx.activity.ComponentActivity
 import androidx.test.core.app.ApplicationProvider
+import com.example.learningtest.R
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,7 +28,6 @@ class ContextBasicTest {
     private val service1 = service1Controller.get()
 
     private val application = ApplicationProvider.getApplicationContext<StubApplication>()
-
 
     @Test
     fun `activity extends ContextWrapper`() {
@@ -49,12 +51,22 @@ class ContextBasicTest {
         (application as ContextWrapper).shouldBeInstanceOf<Context>()
     }
 
+    @Test
+    fun `can get android resources such as string, drawable through context`() {
+        activity1.getString(R.string.app_name) shouldBe "LearningTest"
+        activity1.getDrawable(R.drawable.ic_launcher_foreground) shouldNotBe null
+
+        service1.getString(R.string.app_name) shouldBe "LearningTest"
+        service1.getDrawable(R.drawable.ic_launcher_foreground) shouldNotBe null
+
+        application.getString(R.string.app_name) shouldBe "LearningTest"
+        application.getDrawable(R.drawable.ic_launcher_foreground) shouldNotBe null
+    }
 }
 
 class StubApplication : Application()
 
 private class StubActivity1 : ComponentActivity()
-
 
 private class StubService1 : Service() {
     override fun onBind(intent: Intent?): IBinder? = null
