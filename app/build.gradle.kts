3105cf9e064627fd07230a501e40f3f68aec1914
sh1mj1 <7wlgns@gmail.com>
2025-01-05 16:28:03 +0900

    build: compose test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    debugImplementation(libs.compose.ui.test.manifest)
+    androidTestImplementation(libs.compose.ui.test.junit4)
 }
