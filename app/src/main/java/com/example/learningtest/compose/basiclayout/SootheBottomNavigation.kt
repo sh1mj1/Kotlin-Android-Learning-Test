c329ca1d09579a6017915b2ab0b608c03354934f
sh1mj1 <7wlgns@gmail.com>
2025-01-13 11:40:25 +0900

    feat(SootheBottomNavigation): Bottom navigation - Material

---
+++ app/src/main/java/com/example/learningtest/compose/basiclayout/SootheBottomNavigation.kt
@@ -0,0 +1,61 @@
+package com.example.learningtest.compose.basiclayout
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Spa
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.learningtest.R
+import com.example.learningtest.ui.theme.LearningTestTheme
+
+@Composable
+fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier,
+    ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Spa,
+                    contentDescription = null,
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_home),
+                )
+            },
+            selected = true,
+            onClick = {},
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null,
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_profile),
+                )
+            },
+            selected = false,
+            onClick = {},
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SootheBottomNavigationPreview() {
+    LearningTestTheme { SootheBottomNavigation() }
+}
