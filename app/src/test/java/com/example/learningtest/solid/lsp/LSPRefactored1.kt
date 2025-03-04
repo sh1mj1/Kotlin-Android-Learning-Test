c60764198ce35dbe9e00787be519f6844a3b5665
sh1mj1 <7wlgns@gmail.com>
2025-03-04 20:16:02 +0900

    feat(LSPRefactored): refactored LSP
    remove is-A relation between Square and Rectangle

---
+++ app/src/test/java/com/example/learningtest/solid/lsp/LSPRefactored1.kt
@@ -0,0 +1,92 @@
+package com.example.learningtest.solid.lsp
+
+class LSPRefactored1 {
+    class Customer {
+        fun buyLotto(
+            money: Int,
+            lottoSeller: LottoSeller,
+        ): List<Lottery> = lottoSeller.soldLotto(money)
+    }
+
+    abstract class LottoSeller {
+        abstract val lottoPrice: Int
+
+        fun soldLotto(money: Int): List<Lottery> {
+            val count = money / lottoPrice
+            return List(count) { RandomSquareLottoGenerateStrategy().lotto() }
+        }
+    }
+
+    class NormalLottoSeller : LottoSeller() {
+        override val lottoPrice: Int = LOTTO_PRICE
+
+        companion object {
+            private const val LOTTO_PRICE = 1000
+        }
+    }
+
+    class DisCountLottoSeller : LottoSeller() {
+        override val lottoPrice: Int = LOTTO_PRICE
+
+        companion object {
+            private const val LOTTO_PRICE = 500
+        }
+    }
+
+    data class Lottery(
+        val numbers: List<Int>,
+        val rectangle: Shape,
+    ) {
+        init {
+            numbers.forEach {
+                require(numbers.size == NUMBER_COUNT) {
+                    "Invalid lotto number count"
+                }
+                require(it in MIN_NUMBER..MAX_NUMBER) {
+                    "Invalid lotto number"
+                }
+            }
+        }
+
+        companion object {
+            private const val MIN_NUMBER = 1
+            private const val MAX_NUMBER = 45
+            val numberRange = (MIN_NUMBER..MAX_NUMBER)
+
+            const val NUMBER_COUNT = 6
+        }
+    }
+
+    interface Shape {
+        fun area(): Int
+    }
+
+    class Rectangle(private var width: Int, private var height: Int) : Shape {
+        fun setWidth(width: Int) {
+            this.width = width
+        }
+
+        fun setHeight(height: Int) {
+            this.height = height
+        }
+
+        override fun area() = width * height
+    }
+
+    class Square(private var side: Int) : Shape {
+        fun setSide(side: Int) {
+            this.side = side
+        }
+
+        override fun area() = side * side
+    }
+
+
+    class RandomSquareLottoGenerateStrategy {
+        fun lotto(): Lottery =
+            Lottery(
+                numbers = (Lottery.numberRange).shuffled().take(Lottery.NUMBER_COUNT).sorted(),
+                Square(3),
+            )
+    }
+}
---
+++ app/src/test/java/com/example/learningtest/solid/lsp/LSPRefactored1Test.kt
@@ -0,0 +1,50 @@
+package com.example.learningtest.solid.lsp
+
+import com.example.learningtest.solid.lsp.LSPRefactored1.Rectangle
+import com.example.learningtest.solid.lsp.LSPRefactored1.Square
+import com.example.learningtest.solid.lsp.LSPRefactored1.Shape
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class LSPRefactored1Test : BehaviorSpec({
+
+    Given("가로 4, 세로 5인 직사각형(Rectangle)") {
+        val rectangle = Rectangle(4, 5)
+
+        Then("면적은 20이다") {
+            rectangle.area() shouldBe 20
+        }
+
+        When("너비를 6으로 변경하면") {
+            rectangle.setWidth(6)
+
+            Then("면적은 30이 된다") {
+                rectangle.area() shouldBe 30
+            }
+
+            Then("Shape 타입으로도 면적이 18이 된다") {
+                (rectangle as Shape).area() shouldBe 30
+            }
+        }
+    }
+
+    Given("한 변의 길이가 5인 정사각형(Square)") {
+        val square = Square(5)
+
+        Then("면적은 25이다") {
+            square.area() shouldBe 25
+        }
+
+        When("한 변의 길이를 4로 변경하면") {
+            square.setSide(4)
+
+            Then("면적은 16이 된다") {
+                square.area() shouldBe 16
+            }
+
+            Then("Shape 타입으로도 면적이 16이 된다") {
+                (square as Shape).area() shouldBe 16
+            }
+        }
+    }
+})
