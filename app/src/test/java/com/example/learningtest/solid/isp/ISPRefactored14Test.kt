8c6ce9e0577aa84b2c4c74822fc96c7ebcb74ba9
sh1mj1 <7wlgns@gmail.com>
2025-03-05 15:17:37 +0900

    test(ISPRefactored15Test): implement LottoSeller with chat and reset capabilities

---
+++ app/src/test/java/com/example/learningtest/solid/isp/ISPRefactored14Test.kt
@@ -0,0 +1,137 @@
+package com.example.learningtest.solid.isp
+
+import com.example.learningtest.solid.isp.ISPRefactored14.ChatbotLottoVendingMachine
+import com.example.learningtest.solid.isp.ISPRefactored14.DefaultChatResetCapable
+import com.example.learningtest.solid.isp.ISPRefactored14.DisCountLottoSeller
+import com.example.learningtest.solid.isp.ISPRefactored14.HelloChat
+import com.example.learningtest.solid.isp.ISPRefactored14.MorningChat
+import com.example.learningtest.solid.isp.ISPRefactored14.NoisyLottoVendingMachine
+import com.example.learningtest.solid.isp.ISPRefactored14.NoisyReset
+import com.example.learningtest.solid.isp.ISPRefactored14.NormalLottoSeller
+import com.example.learningtest.solid.isp.ISPRefactored14.NormalLottoVendingMachine
+import com.example.learningtest.solid.isp.ISPRefactored14.QuietReset
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class ISPRefactored14Test : BehaviorSpec({
+
+    Given("ChatCapable 인터페이스를 구현한 MorningChat") {
+        val morningChat = MorningChat()
+
+        When("onRest()를 호출하면") {
+            val result = morningChat.onRest()
+
+            Then("Chatting -- Good morning! 이 출력된다") {
+                result shouldBe "Chatting -- Good morning!"
+            }
+        }
+    }
+
+    Given("ChatCapable 인터페이스를 구현한 HelloChat") {
+        val helloChat = HelloChat()
+
+        When("onRest()를 호출하면") {
+            val result = helloChat.onRest()
+
+            Then("Chatting -- Hello! 이 출력된다") {
+                result shouldBe "Chatting -- Hello!"
+            }
+        }
+    }
+
+    Given("ResetCapable 인터페이스를 구현한 QuietReset") {
+        val quietReset = QuietReset()
+
+        When("onRest()를 호출하면") {
+            val result = quietReset.onRest()
+
+            Then("Resetting -- Quietly 가 출력된다") {
+                result shouldBe "Resetting -- Quietly"
+            }
+        }
+    }
+
+    Given("ResetCapable 인터페이스를 구현한 NoisyReset") {
+        val noisyReset = NoisyReset()
+
+        When("onRest()를 호출하면") {
+            val result = noisyReset.onRest()
+
+            Then("Resetting -- With noise 가 출력된다") {
+                result shouldBe "Resetting -- With noise"
+            }
+        }
+    }
+
+    Given("ChatResetCapable 인터페이스를 구현한 DefaultChatResetCapable") {
+        val chatbot = DefaultChatResetCapable()
+
+        When("onRest()를 호출하면") {
+            val result = chatbot.onRest()
+
+            Then("Chatting -- Hello, You can call me chatbot! \nResetting -- Quietly 가 출력된다") {
+                result shouldBe "Chatting -- Hello, You can call me chatbot!\nResetting -- Quietly"
+            }
+        }
+    }
+
+    Given("NormalLottoSeller") {
+        val normalLottoSeller = NormalLottoSeller()
+
+        When("onRest()를 호출하면") {
+            val result = normalLottoSeller.onRest()
+
+            Then("Chatting -- Hello! 가 출력된다") {
+                result shouldBe "Chatting -- Hello!"
+            }
+        }
+    }
+
+    Given("DisCountLottoSeller") {
+        val discountLottoSeller = DisCountLottoSeller()
+
+        When("onRest()를 호출하면") {
+            val result = discountLottoSeller.onRest()
+
+            Then("Chatting -- Good morning! 가 출력된다") {
+                result shouldBe "Chatting -- Good morning!"
+            }
+        }
+    }
+
+    Given("NormalLottoVendingMachine") {
+        val normalLottoVendingMachine = NormalLottoVendingMachine()
+
+        When("onRest()를 호출하면") {
+            val result = normalLottoVendingMachine.onRest()
+
+            Then("Resetting -- Quietly 가 출력된다") {
+                result shouldBe "Resetting -- Quietly"
+            }
+        }
+    }
+
+    Given("NoisyLottoVendingMachine") {
+        val noisyLottoVendingMachine = NoisyLottoVendingMachine()
+
+        When("onRest()를 호출하면") {
+            val result = noisyLottoVendingMachine.onRest()
+
+            Then("Resetting -- With noise 가 출력된다") {
+                result shouldBe "Resetting -- With noise"
+            }
+        }
+    }
+
+    Given("ChatbotLottoVendingMachine") {
+        val chatbotLottoVendingMachine = ChatbotLottoVendingMachine()
+
+        When("onRest()를 호출하면") {
+            val result = chatbotLottoVendingMachine.onRest()
+
+            Then("Chatting -- Hello, You can call me chatbot! \nResetting -- Quietly 가 출력된다") {
+                result shouldBe "Chatting -- Hello, You can call me chatbot!\nResetting -- Quietly"
+            }
+        }
+    }
+})
