f8fd8936798cdb065f16108cbf897fc28cd03677
sh1mj1 <7wlgns@gmail.com>
2025-04-22 17:29:46 +0900

    test(SortingTest): add tests for sortedWith functionality with various comparators

--- app/src/test/java/com/example/learningtest/collection/functional/SortingTest.kt
+++ app/src/test/java/com/example/learningtest/collection/functional/SortingTest.kt
@@ -5,6 +5,7 @@ import io.kotest.matchers.collections.shouldBeSorted
 import io.kotest.matchers.collections.shouldBeSortedBy
 import io.kotest.matchers.collections.shouldBeSortedDescending
 import io.kotest.matchers.collections.shouldBeSortedDescendingBy
+import io.kotest.matchers.collections.shouldBeSortedWith
 import io.kotest.matchers.shouldBe
 
 class SortingTest : FreeSpec({
@@ -61,6 +62,129 @@ class SortingTest : FreeSpec({
                     User("B", 20),
                 )
             sorted shouldBeSortedDescendingBy { it.age }
+        }
+
+        "sortedWith 는 Comparator 를 사용하여 정렬" - {
+            "sortedWith - 기본" {
+                val users: List<User> =
+                    listOf(
+                        User("Alice", 30),
+                        User("Cathy", 22),
+                        User("Bob", 28),
+                    )
+                val sortedByAge1: List<User> = users.sortedWith(compareBy<User> { it.age })
+                val sortedByAge2: List<User> =
+                    users.sortedWith { user1, user2 ->
+                        user1.age.compareTo(user2.age)
+                    }
+                val sortedByAge3: List<User> =
+                    users.sortedWith { user1, user2 ->
+                        user1.age - user2.age
+                    }
+
+                val expected: List<User> =
+                    listOf(
+                        User("Cathy", 22),
+                        User("Bob", 28),
+                        User("Alice", 30),
+                    )
+
+                sortedByAge1 shouldBe expected
+                sortedByAge2 shouldBe expected
+                sortedByAge3 shouldBe expected
+
+                sortedByAge1.shouldBeSortedWith(compareBy<User> { it.age })
+                sortedByAge2.shouldBeSortedWith(compareBy<User> { it.age })
+                sortedByAge3.shouldBeSortedWith(compareBy<User> { it.age })
+            }
+
+            "sortedWith - 이름이 같을 경우 입력 순서 유지 (Stable Sort)" {
+                val users: List<User> =
+                    listOf(
+                        User("Alice", 30),
+                        User("Bob", 25),
+                        User("Alice", 22),
+                    )
+                val sortedByName1: List<User> = users.sortedWith(compareBy<User> { it.name })
+                val sortedByName2: List<User> =
+                    users.sortedWith { user1, user2 ->
+                        user1.name.compareTo(user2.name)
+                    }
+                /* String 타입의 비교는 - 연산으로 불가능하다.
+                val sortedByName3 : List<User> = users.sortedWith { user1, user2 ->
+                    user1.name - user2.name
+                }
+                 */
+
+                val expected: List<User> =
+                    listOf(
+                        User("Alice", 30),
+                        User("Alice", 22),
+                        User("Bob", 25),
+                    )
+
+                sortedByName1 shouldBe expected
+                sortedByName2 shouldBe expected
+
+                sortedByName1 shouldBeSortedWith (compareBy<User> { it.name })
+                sortedByName2 shouldBeSortedWith (compareBy(User::name))
+            }
+
+            "sortedWith + compareByDescending" {
+                val users: List<User> =
+                    listOf(
+                        User("Alice", 30),
+                        User("Cathy", 22),
+                        User("Bob", 28),
+                    )
+
+                val sortedByDescendingAge1: List<User> =
+                    users.sortedWith(
+                        compareByDescending<User> {
+                            it.age
+                        },
+                    )
+                val sortedByDescendingAge2: List<User> =
+                    users.sortedWith { user1, user2 ->
+                        user2.age.compareTo(user1.age)
+                    }
+                val sortedByDescendingAge3: List<User> =
+                    users.sortedWith { user1, user2 ->
+                        user2.age - user1.age
+                    }
+
+                val expected: List<User> =
+                    listOf(
+                        User("Alice", 30),
+                        User("Bob", 28),
+                        User("Cathy", 22),
+                    )
+
+                sortedByDescendingAge1 shouldBe expected
+                sortedByDescendingAge2 shouldBe expected
+                sortedByDescendingAge3 shouldBe expected
+
+                sortedByDescendingAge1 shouldBeSortedWith (compareByDescending(User::age))
+                sortedByDescendingAge2 shouldBeSortedWith (compareByDescending(User::age))
+                sortedByDescendingAge3 shouldBeSortedWith (compareByDescending(User::age))
+            }
+
+            "sortedWith - 이름으로 오름차순 정렬, 이름이 같다면 나이로 오름차순 정렬" {
+                val users: List<User> =
+                    listOf(
+                        User("Alice", 30),
+                        User("Cathy", 22),
+                        User("Alice", 28),
+                    )
+
+                val sorted1: List<User> =
+                    users.sortedWith(compareBy<User> { it.name }.thenBy { it.age })
+                val sorted2: List<User> = users.sortedWith(compareBy(User::name).thenBy(User::age))
+                val sorted3: List<User> =
+                    users.sortedWith { user1, user2 ->
+                        user1.name.compareTo(user2.name)
+                    }
+            }
         }
 
         "reversed - 순서만 뒤집기 (뒤집으면서 정렬하지는 않음)" {
