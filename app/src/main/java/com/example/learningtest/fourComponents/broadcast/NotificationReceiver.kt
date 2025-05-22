67dc595da909b06ec7000f8c9e6e8d5bd497876f
sh1mj1 <7wlgns@gmail.com>
2025-05-22 11:02:10 +0900

    feat(NotificationReceiver): 알림 클릭 시 앱 실행하도록 PendingIntent 업데이트

--- app/src/main/java/com/example/learningtest/fourComponents/broadcast/NotificationReceiver.kt
+++ app/src/main/java/com/example/learningtest/fourComponents/broadcast/NotificationReceiver.kt
@@ -2,6 +2,7 @@ package com.example.learningtest.fourComponents.broadcast
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -25,11 +26,24 @@ class NotificationReceiver : BroadcastReceiver() {
             )
         notificationManager.createNotificationChannel(channel)
 
+        val activityIntent =
+            Intent(context, AlarmActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+        val pendingIntent =
+            PendingIntent.getActivity(
+                context,
+                0,
+                activityIntent,
+                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
+            )
+
         val notification =
             NotificationCompat.Builder(context, "alarm_channel_id")
                 .setSmallIcon(android.R.drawable.ic_dialog_info)
                 .setContentTitle("출고 예정 알림")
                 .setContentText("출고 30분 전입니다. 확인해주세요.")
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
                 .build()
 
